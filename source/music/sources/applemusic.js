56e7aded51d99fd73db78a9ff8d2c9c1b9adc1f1
SooyEric <soooyeric@gmail.com>
2026-08-12 23:41:36 -0600

    Update applemusic.js

--- source/music/sources/applemusic.js
+++ source/music/sources/applemusic.js
@@ -1,1 +1,21 @@
-s
+import { AppleMusicExtractor } from '@discord-player/extractor';
+import { logger } from '../../utils/logger.js';
+
+export async function registerAppleMusicExtractor(player) {
+    try {
+        await player.extractors.register(AppleMusicExtractor, {
+            searchLimit: 5
+        });
+
+        logger.success(
+            'Extractor de Apple Music registrado.'
+        );
+    } catch (error) {
+        logger.error(
+            'No se pudo registrar el extractor de Apple Music.',
+            error
+        );
+
+        throw error;
+    }
+}
