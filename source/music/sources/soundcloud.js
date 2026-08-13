89fd8ddf81116c2d3381a8d646bd6cdc4b70ad2c
SooyEric <soooyeric@gmail.com>
2026-08-12 23:41:25 -0600

    Update soundcloud.js

--- source/music/sources/soundcloud.js
+++ source/music/sources/soundcloud.js
@@ -1,1 +1,19 @@
-s
+import { SoundCloudExtractor } from '@discord-player/extractor';
+import { logger } from '../../utils/logger.js';
+
+export async function registerSoundCloudExtractor(player) {
+    try {
+        await player.extractors.register(SoundCloudExtractor, {
+            searchLimit: 5
+        });
+
+        logger.success('Extractor de SoundCloud registrado.');
+    } catch (error) {
+        logger.error(
+            'No se pudo registrar el extractor de SoundCloud.',
+            error
+        );
+
+        throw error;
+    }
+}
