3434962e9082ce648b469af5e91e55c36c89a7d4
SooyEric <soooyeric@gmail.com>
2026-08-12 23:39:39 -0600

    Update queue.js

--- source/music/queue.js
+++ source/music/queue.js
@@ -1,1 +1,78 @@
-s
+export class MusicQueue {
+    constructor(guildId) {
+        this.guildId = guildId;
+
+        this.current = null;
+        this.tracks = [];
+    }
+
+    add(track) {
+        if (!track) {
+            return false;
+        }
+
+        this.tracks.push(track);
+
+        return true;
+    }
+
+    addMany(tracks) {
+        if (!Array.isArray(tracks) || tracks.length === 0) {
+            return 0;
+        }
+
+        const validTracks = tracks.filter(Boolean);
+
+        this.tracks.push(...validTracks);
+
+        return validTracks.length;
+    }
+
+    next() {
+        return this.tracks.shift() || null;
+    }
+
+    peek() {
+        return this.tracks[0] || null;
+    }
+
+    remove(index) {
+        if (
+            !Number.isInteger(index) ||
+            index < 0 ||
+            index >= this.tracks.length
+        ) {
+            return null;
+        }
+
+        return this.tracks.splice(index, 1)[0] || null;
+    }
+
+    clear() {
+        this.tracks = [];
+    }
+
+    setCurrent(track) {
+        this.current = track || null;
+    }
+
+    getCurrent() {
+        return this.current;
+    }
+
+    get size() {
+        return this.tracks.length;
+    }
+
+    get isEmpty() {
+        return this.tracks.length === 0;
+    }
+
+    get all() {
+        return [...this.tracks];
+    }
+
+    hasTracks() {
+        return this.tracks.length > 0;
+    }
+}
