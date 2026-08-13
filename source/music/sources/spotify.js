f06dec03cd929564b2d355ffe40698632cc871f4
SooyEric <soooyeric@gmail.com>
2026-08-13 01:11:42 -0600

    Update spotify.js

--- source/music/sources/spotify.js
+++ source/music/sources/spotify.js
@@ -1,19 +1,10 @@
 import { SpotifyExtractor } from '@discord-player/extractor';
-import { logger } from '../../utils/logger.js';
 
 export async function registerSpotifyExtractor(player) {
-    try {
-        await player.extractors.register(SpotifyExtractor, {
+    await player.extractors.register(
+        SpotifyExtractor,
+        {
             searchLimit: 5
-        });
-
-        logger.success('Extractor de Spotify registrado.');
-    } catch (error) {
-        logger.error(
-            'No se pudo registrar el extractor de Spotify.',
-            error
-        );
-
-        throw error;
-    }
+        }
+    );
 }
