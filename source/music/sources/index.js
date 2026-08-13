4f4dd8b3b31f13a72268bc2e28d724b23435f87f
SooyEric <soooyeric@gmail.com>
2026-08-12 23:41:52 -0600

    Update index.js

--- source/music/sources/index.js
+++ source/music/sources/index.js
@@ -1,1 +1,11 @@
-s
+import { registerYouTubeExtractor } from './youtube.js';
+import { registerSpotifyExtractor } from './spotify.js';
+import { registerSoundCloudExtractor } from './soundcloud.js';
+import { registerAppleMusicExtractor } from './applemusic.js';
+
+export async function registerExtractors(player) {
+    await registerYouTubeExtractor(player);
+    await registerSpotifyExtractor(player);
+    await registerSoundCloudExtractor(player);
+    await registerAppleMusicExtractor(player);
+}
