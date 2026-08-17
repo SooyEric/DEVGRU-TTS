72fe2f27ea4a581e682ce3b21cfdecfb2d116662
SooyEric <soooyeric@gmail.com>
2026-08-16 21:12:34 -0600

    Delete source/events directory

--- source/events/error.js
+++
@@ -1,6 +0,0 @@
-export default function error(error) {
-    console.error(
-        '❌ Error de Discord:',
-        error
-    );
-}
--- source/events/ready.js
+++
@@ -1,3 +0,0 @@
-export default async function ready(client) {
-    console.log(`✅ DEVGRU-TTS conectado como ${client.user.tag}`);
-}
